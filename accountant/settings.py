7b206ef6e3e98a33c4631fe48995bba4991cbbc6
Christos Miniotis <accounts@chrismin13.com>
2023-12-13 16:03:48 +0200

    Messed up my first pythonanywhere account, trying another one

--- accountant/settings.py
+++ accountant/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-cfrhsjxgejrt(k0b!=z@p8pk#6-)mg5fc)@ozjh3o=e@u-&ra5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chrismin13.pythonanywhere.com']
+ALLOWED_HOSTS = ['cminiotis.pythonanywhere.com']
 
 
 # Application definition
